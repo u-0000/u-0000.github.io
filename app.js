a761a3bbbe4bf2627564af3043aef8c1656bacea
u-0000 <63988411+u-0000@users.noreply.github.com>
2021-12-10 03:20:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ function youtubeUrlParser(url) {
 function submitForm(that) {
   youtubeUrlParser(that.searchTerm.value)
 }
-
-if (window.location.search.indexOf('v=') > -1) {
-    youtubeUrlParser(`https://www.youtube.com/watch?v=${window.location.search.split('v=')[1]}`)
-}
+document.addEventListener("DOMContentLoaded",function(){
+	if (window.location.search.indexOf('v=') > -1) {
+	    youtubeUrlParser(`https://www.youtube.com/watch?v=${window.location.search.split('v=')[1]}`)
+	}
+});
